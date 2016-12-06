164df77f3bf95b645295e08b90112b0c68b65e85
Dominik Zinser <d.zinser@mwaysolutions.com>
2016-12-06 16:48:48 +0100

    eslint and Editor config added

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
@@ -5,62 +6,64 @@ var multiLanguage = require('metalsmith-multi-language');
 var layouts = require('metalsmith-layouts');
 var gulp = require('gulp');
 var connect = require('gulp-connect');
-var fs = require('fs');
 var livereload = require('gulp-livereload');
-var gulp_metalsmith = require('gulp-metalsmith');
+var gulpMetalsmith = require('gulp-metalsmith');
 var sass = require('gulp-sass');
 var runSequence = require('run-sequence');
 var del = require('del');
-var handlebars = require("handlebars");
+var handlebars = require('handlebars');
 var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
+var eslint = require('gulp-eslint');
 
-handlebars.registerHelper('if_eq', function(a, b, opts) {
-    if (a === b) {
-        return opts.fn(this);
-    } else {
-        return opts.inverse(this);
-    }
+handlebars.registerHelper('if_eq', function (a, b, opts) {
+  if (a === b) {
+    return opts.fn(this);
+  } else {
+    return opts.inverse(this);
+  }
 });
 
 var dir = {
   base: __dirname + '/',
   lib: __dirname + '/lib/',
-  to_smith_source: './src/to_smith/',
+  toMetalsmithSource: './src/to_smith/',
   dest: './build/',
-  scss_source: './src/style/scss/',
-  assets_source: './src/assets/'
+  scssSource: './src/style/scss/',
+  assetsSource: './src/assets/',
+  jsSource: './src/lib/'
 };
 
 var templateConfig = {
   engine: 'handlebars',
-  directory: dir.to_smith_source + 'templates/',
-  partials: dir.to_smith_source + 'partials/',
+  directory: dir.toMetalsmithSource + 'templates/',
+  partials: dir.toMetalsmithSource + 'partials/',
   default: 'page.html'
 };
 
-var ms = metalsmith(__dirname) // the working directory
+metalsmith(__dirname) // the working directory
 .clean(true)            // clean the build directory
-.source(dir.to_smith_source + 'content/')    // the page source directory
+.source(dir.toMetalsmithSource + 'content/')    // the page source directory
 .destination(dir.dest)  // the destination directory
 .use(markdown())        // convert markdown to HTML
-.use(multiLanguage({ 
-    default: 'es', 
-    locales: [
-        'en', 
-        'es'
-        ] 
-    }
+.use(multiLanguage({
+  default: 'es',
+  locales: [
+    'en',
+    'es'
+  ]}
 ))
 .use(layouts(templateConfig))
 .use(debug(false))
-.build(function(err) {  // build the site
-    if (err) throw err;   // and throw errors
+.build( function (err) {  // build the site
+  if (err) {
+    throw err;   // and throw errors
+  }
 });
 
 
-function debug(logToConsole) {
-  return function(files, metalsmith, done) {
+function debug (logToConsole) {
+  return function (files, metalsmith, done) {
     if (logToConsole) {
       console.log('\nMETADATA:');
       console.log(metalsmith.metadata());
@@ -73,37 +76,41 @@ function debug(logToConsole) {
 
     done();
   };
-};
+}
 
 //-----------------------------------------------------------------
 
-var source_files = function() {
-  return gulp.src(dir.to_smith_source + '**/*').pipe( gulp_metalsmith() ).pipe( gulp.dest(dir.dest) ).pipe( livereload() );
+var sourceFiles = function () {
+  return gulp.src(dir.toMetalsmithSource + '**/*').pipe( gulpMetalsmith() ).pipe( gulp.dest(dir.dest) ).pipe( livereload() );
 };
 
-var styles = function() {
-    return gulp.src(dir.scss_source + '**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('sourcemaps'))
+var styles = function () {
+  return gulp.src(dir.scssSource + '**/*.scss')
+    .pipe( sourcemaps.init() )
+    .pipe( sass().on('error', sass.logError) )
+    .pipe( sourcemaps.write('sourcemaps') )
     .pipe( gulp.dest(dir.dest + 'style/css') )
-    .pipe( livereload());
+    .pipe( livereload() );
 };
 
-gulp.task('files-watch', source_files);
+gulp.task('files-watch', sourceFiles);
 gulp.task('styles-watch', styles);
 
-gulp.task('clean-build', function(){
-    del([
-        dir.dest + 'content',
-        dir.dest + 'partials',
-        dir.dest + 'assets',
-        dir.dest + 'templates'
-    ]);
+gulp.task('clean-build', function () {
+  del([
+    dir.dest + 'content',
+    dir.dest + 'partials',
+    dir.dest + 'assets',
+    dir.dest + 'templates'
+  ]);
 });
 
-gulp.task('image-min', function(){
-    gulp.src(dir.assets_source + 'images/**/*').pipe( imagemin() ).pipe(gulp.dest(dir.dest + "assets/images"));
+gulp.task('image-min', function () {
+  gulp.src(dir.assetsSource + 'images/**/*').pipe( imagemin() ).pipe(gulp.dest(dir.dest + 'assets/images'));
+});
+
+gulp.task('eslint', function () {
+  gulp.src(dir.jsSource + '*.js').pipe( eslint() ).pipe( eslint.format() );
 });
 
 gulp.task('server', function () {
@@ -111,13 +118,13 @@ gulp.task('server', function () {
     root: [dir.dest],
     port: 8000,
     livereload: true,
-    middleware: function(connect, opt) {
-        return [
-            connect.static(dir.dest),
-            function(req, res, next) {
-                next();
-            }
-        ];
+    middleware: function (connect) {
+      return [
+        connect.static(dir.dest),
+        function (req, res, next) {
+          next();
+        }
+      ];
     }
   });
 });
@@ -125,8 +132,8 @@ gulp.task('server', function () {
 //-----------------------------------------------------------------
 
 gulp.task('watch', function () {
-    livereload.listen();
-    runSequence('files-watch', 'styles-watch', 'clean-build', 'image-min'); //1 2 3
+  livereload.listen();
+  runSequence('files-watch', 'styles-watch', 'clean-build', 'image-min', 'eslint'); //1 2 3
 });
 
 //-----------------------------------------------------------------
---
+++ src/lib/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log('hello world');
+
+function whatatest () {
+  console.log('test');
+}
+
+whatatest();
